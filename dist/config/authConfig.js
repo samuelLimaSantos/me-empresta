a8f87571e0478a6bd97d481ecedf81ade472f725
Samuel Santos <samuellima280499@gmail.com>
2020-12-13 03:22:23 -0300

    Preparing for deploy

--- dist/config/authConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        expiresIn: '1d',
-    }
-};
-//# sourceMappingURL=authConfig.js.map
--- dist/database/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var typeorm_1 = require("typeorm");
-typeorm_1.createConnection();
-//# sourceMappingURL=index.js.map
