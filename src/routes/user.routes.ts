94463df3fc86bab61c6737fb5fcbfcc3ffaaf7a1
Samuel Santos <samuellima280499@gmail.com>
2020-12-13 03:05:36 -0300

    Preparing for deploy

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -10,5 +10,8 @@ const routes = Router();
 const userController = new UserController();
 
 routes.post('/', upload.single('photo'), userController.create);
+routes.get('/', (request, response) => {
+  return response.json({message: 'esperance'});
+})
 
 export default routes;
