95b82a01c8c56fcba21bb11e6e5f83e7600fbb89
Samuel Santos <samuellima280499@gmail.com>
2020-12-16 19:42:54 -0300

    teste

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,6 +1,8 @@
 import * as multer from 'multer';
 import * as path from 'path';
 import * as crypto from 'crypto';
+import * as aws from 'aws-sdk';
+import * as multerS3 from 'multer-s3';
 
 export default {
   storage: multer.diskStorage({
@@ -13,4 +15,17 @@ export default {
       callback(null, fileName);
     },
   }),
+  // s3: multerS3({
+  //   s3: new aws.S3(),
+  //   bucket: process.env.BUCKET_NAME,
+  //   contentType: multerS3.AUTO_CONTENT_TYPE,
+  //   acl: "public-read",
+  //   key: (request, file, callback) => {
+  //     const hash = crypto.randomBytes(6).toString('hex');
+
+  //     const fileName = `${hash}-${file.originalname}`;
+
+  //     callback(null, fileName);
+  //   }
+  // })
 };
