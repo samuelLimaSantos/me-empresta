885440a28e488fec5916771335a13e034b94fdce
Samuel Santos <samuellima280499@gmail.com>
2021-06-18 00:40:58 -0300

    test

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -5,6 +5,12 @@ describe("Users",() => {
     const number = 0;
 
     expect(number).toBe(0);
-  })
+  });
+
+  it("should be able to pass the second test", () => {
+    const number = 1;
+
+    expect(number).toBe(2);
+  });
 
 });
