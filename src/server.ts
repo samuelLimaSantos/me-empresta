07483805d7f0ed3360aa65b236848f7175ab718f
Samuel Santos <samuellima280499@gmail.com>
2021-06-17 23:52:12 -0300

    consertando a merda que fiz

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,5 @@
-import * as express from 'express';
-const cors = require('cors');
-import * as path from 'path';
-import routes from './routes';
-import * as dotenv from 'dotenv';
-import './database';
-import 'reflect-metadata';
 
-
-const app = express();
-dotenv.config()
-
-app.use(cors());
-app.use(express.json());
-
-app.use(routes);
-
-
-app.use('/uploads', express.static(path.join(__dirname, "..", 'uploads')));
+import { app } from './app';
 
 const port = 3333;
 
